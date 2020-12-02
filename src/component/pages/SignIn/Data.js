65eb58033c178c5cbed3e6780e04c221ee270140
Gavintra <75388102+Gavintra@users.noreply.github.com>
2020-12-03 01:32:36 +0700

    Add files via upload

---
+++ src/component/pages/SignIn/Data.js
@@ -0,0 +1,12 @@
+export const homeObjOne = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: '',
+    headline: 'ระบบจัดการเอกสาร',
+    description: 'มุ่งมั่นบูรณาการ ประสานงานเป็นเลิศ เชิดชูคุณธรรม ผู้นำความมั่นคง ดำรงความสามัคคี',
+    buttonLabel: '',
+    imgStart: '',
+    img: 'images/book.png',
+    alt: 'Images'
+};
